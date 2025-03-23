a355ce4a2f7fa625fb5391e5b13619a09ca1988b
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-23 22:00:09 +0100

    Add files via upload

--- F_taste_rete_neurale/__init__.py
+++ F_taste_rete_neurale/__init__.py
@@ -25,7 +25,7 @@ from logging import getLogger
 def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('F_taste_rete_neurale.config.DevelopmentConfig')
+    app.config.from_object('F_taste_rete_neurale.configurazione.DevelopmentConfig')
 
     
    
